5f5215b42f103b05766478bdf032e35b01672ead
Sharmina Akhtar <sakht318@gmail.com>
2018-04-18 23:22:29 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,25 @@
 class SongsController < ApplicationController
 
+  def show
+    @song = Song.find(params[:id])
+  end
+
+  def new
+    @song = Song.new
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+  
   
 
 end
