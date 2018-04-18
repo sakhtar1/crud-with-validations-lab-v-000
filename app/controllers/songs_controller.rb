e6e47683c853a23404b28fbc4c7ab6f59520a41f
Sharmina Akhtar <sakht318@gmail.com>
2018-04-18 23:26:23 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -13,6 +13,13 @@ class SongsController < ApplicationController
   end
 
   def create
+    @song = Song.new(song_params)
+    if @song.valid?
+      @song.save
+      redirect_to song_path(@song)
+    else
+      render :new
+    end
 
   end
 
