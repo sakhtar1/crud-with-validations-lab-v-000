ab1c5e71be2808cd0df376244b605f8fcbc37d3c
Sharmina Akhtar <sakht318@gmail.com>
2018-04-18 23:25:51 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -17,14 +17,21 @@ class SongsController < ApplicationController
   end
 
   def update
-
+    @song = Song.find(params[:id])
+    if @song.update(song_params)
+      @song.valid?
+      @song.save
+      redirect_to song_path(@song)
+    else
+      render :edit
+    end
   end
 
   def destroy
     Song.find(params[:id]).destroy
     redirect_to song_url
   end
-  
+
   private
 
   def song_params
