473714e2fb034e4dbd665c9c8f5150a3fd3be6ca
Sharmina Akhtar <sakht318@gmail.com>
2018-04-19 00:29:08 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -7,14 +7,14 @@ class Song < ActiveRecord::Base
   validates :release_year, presence: true
   validates :artist_name, presence: true
   with_options if: :released? do |song|
-  song.validates :release_year, presence: true
-  song.validates :release_year, numericality: {
-    less_than_or_equal_to: Date.today.year
-  }
-end
+    song.validates :release_year, presence: true
+    song.validates :release_year, numericality: {
+      less_than_or_equal_to: Date.today.year
+    }
+  end
 
-def released?
-  released
-end
-end
+  def released?
+    released
+  end
+
 end
