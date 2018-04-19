9614c1730743647930f67219e8bf61ea944243fb
Sharmina Akhtar <sakht318@gmail.com>
2018-04-19 00:40:23 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -43,7 +43,7 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.permit(:title, :released, :release_year, :artist_name, :genre)
+    arams.require(:song).permit(:title, :released, :release_year, :artist_name, :genre)
   end
 
 
