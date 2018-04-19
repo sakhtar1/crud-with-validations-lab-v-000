41015a1ceb181bf58030783d393ec65ae9bb17d4
Sharmina Akhtar <sakht318@gmail.com>
2018-04-19 00:28:55 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,7 +1,20 @@
 class Song < ActiveRecord::Base
-  validates :title, presence: true, uniqueness: true
-  validates :released, inclusion: { in: %w(True False),
-   message: "%{value} is not a valid response" }
+  validates :title, presence: true
+  validates :title, uniqueness: {
+    scope: [:release_year, :artist_name],
+    message: "No repition by same artist"}
+  validates :released, inclusion: { in: [true, false]}
   validates :release_year, presence: true
   validates :artist_name, presence: true
+  with_options if: :released? do |song|
+  song.validates :release_year, presence: true
+  song.validates :release_year, numericality: {
+    less_than_or_equal_to: Date.today.year
+  }
+end
+
+def released?
+  released
+end
+end
 end
